cc78c391aec1cc975d2ff91ee6b2643bd6ddaa61
yangkai <deveye@126.com>
2021-05-24 16:42:01 +0800

    feat: 顺序消息

---
+++ src/main/java/cn/deveye/learning/rocketmq/simple/OrderedProducer.java
@@ -0,0 +1,38 @@
+package cn.deveye.learning.rocketmq.simple;
+
+import cn.deveye.learning.rocketmq.Constants;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
+
+public class OrderedProducer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("ordered_producer");
+        producer.setNamesrvAddr(Constants.NAME_SRV_ADDR);
+        producer.start();
+        String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
+        for (int i = 0; i < 100; i++) {
+            int orderId = i % 10;
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("Topic_test", tags[i % tags.length], "KEY" + i,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    Integer id = (Integer) arg;
+                    int index = id % mqs.size();
+                    return mqs.get(index);
+                }
+            }, orderId);
+
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+}
