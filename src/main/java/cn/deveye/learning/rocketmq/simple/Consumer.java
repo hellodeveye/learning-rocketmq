58b75cf0acdb5b24a35d2da47a32cbaec414b0f7
yangkai <deveye@126.com>
2021-05-24 14:53:26 +0800

    feat: 初学RocketmQ

---
+++ src/main/java/cn/deveye/learning/rocketmq/simple/Consumer.java
@@ -0,0 +1,28 @@
+package cn.deveye.learning.rocketmq.simple;
+
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test_consumer");
+        consumer.setNamesrvAddr("39.105.157.176:9876");
+        consumer.subscribe("Topic_test", "*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> messages, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                System.out.println(messages);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+    }
+
+}
