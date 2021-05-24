ea646bfafaace89f34ae5a621c77dc1494caa47a
yangkai <deveye@126.com>
2021-05-24 14:30:00 +0800

    Initial commit

---
+++ src/main/java/cn/deveye/learning/rocketmq/LearningRocketmqApplication.java
@@ -0,0 +1,13 @@
+package cn.deveye.learning.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningRocketmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/deveye/learning/rocketmq/LearningRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.deveye.learning.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
